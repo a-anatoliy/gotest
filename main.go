45ff0e6f7e3efbe4ea705f73e7211e359b1168d3
Anatol <a3three@gmail.com>
2021-04-12 23:17:57 +0200

    added config module

--- main.go
+++ main.go
@@ -1,104 +1,114 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "html/template"
-  "database/sql"
-  _ "github.com/go-sql-driver/mysql"
+	"database/sql"
+	"fmt"
+	"html/template"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/tkanos/gonfig"
 )
 
 type Article struct {
-  Id uint16
-  Title, Announcement, FullText string
+	Id                            uint16
+	Title, Announcement, FullText string
+}
+
+type Configuration struct {
+	Port   int
+	paswrd string
+	user   string
 }
 
 var posts = []Article{}
 
 func index(w http.ResponseWriter, r *http.Request) {
-  t, err := template.ParseFiles("templates/index.html","templates/header.html","templates/footer.html","templates/top_menu.html")
+	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html", "templates/top_menu.html")
 
-  if err != nil {
-    fmt.Fprintf(w, err.Error())
-  }
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
 
-  db, err := sql.Open("mysql", "root:******@tcp(127.0.0.1:3306)/test_lucky")
-  if err != nil {
-    fmt.Fprintf(w, err.Error())
-  }
+	db, err := sql.Open("mysql", "root:******@tcp(127.0.0.1:3306)/test_lucky")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
 
-  defer db.Close()
+	defer db.Close()
 
-  result, err := db.Query("select * from `articles`")
-  if err != nil {
-    panic(err)
-  }
+	result, err := db.Query("select * from `articles`")
+	if err != nil {
+		panic(err)
+	}
 
-  posts = []Article{}
-  for result.Next() {
-    var post Article
-    err = result.Scan(&post.Id,&post.Title,&post.Announcement,&post.FullText)
-    if err != nil {
-      panic(err)
-    }
+	posts = []Article{}
+	for result.Next() {
+		var post Article
+		err = result.Scan(&post.Id, &post.Title, &post.Announcement, &post.FullText)
+		if err != nil {
+			panic(err)
+		}
 
-    posts = append(posts, post)
-    // fmt.Println(fmt.Sprintf("Post: %s with id: %d", post.Title, post.Id))
-  }
+		posts = append(posts, post)
+		// fmt.Println(fmt.Sprintf("Post: %s with id: %d", post.Title, post.Id))
+	}
 
-  
-
-  t.ExecuteTemplate(w, "index", posts)
+	t.ExecuteTemplate(w, "index", posts)
 }
 
-func create (w http.ResponseWriter, r *http.Request) {
-  t, err := template.ParseFiles("templates/create.html","templates/header.html","templates/footer.html","templates/top_menu.html")
+func create(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/create.html", "templates/header.html", "templates/footer.html", "templates/top_menu.html")
 
-  if err != nil {
-    fmt.Fprintf(w, err.Error())
-  }
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
 
-  t.ExecuteTemplate(w, "create", nil)
+	t.ExecuteTemplate(w, "create", nil)
 }
 
-func save_article (w http.ResponseWriter, r *http.Request) {
-  title := r.FormValue("title")
-  announcement := r.FormValue("announcement")
-  full_text := r.FormValue("full_text")
+func save_article(w http.ResponseWriter, r *http.Request) {
+	title := r.FormValue("title")
+	announcement := r.FormValue("announcement")
+	full_text := r.FormValue("full_text")
 
-  if title == "" || announcement == "" || full_text == "" {
-    fmt.Fprintf(w,"Required parameters not set")
-  } else {
-    db, err := sql.Open("mysql", "root:******@tcp(127.0.0.1:3306)/test_lucky")
-    if err != nil {
-      fmt.Fprintf(w, err.Error())
-      // panic(err)
-    }
-  
-    defer db.Close()
-  
-    insert, err := db.Query(fmt.Sprintf("insert into `articles` (`title`,`announcement`,`full_text`) values ('%s','%s','%s')",title,announcement,full_text))
-    if err != nil {
-      fmt.Fprintf(w, err.Error())
-      // panic(err)
-    }
-  
-    defer insert.Close()
-  }
+	if title == "" || announcement == "" || full_text == "" {
+		fmt.Fprintf(w, "Required parameters not set")
+	} else {
+		db, err := sql.Open("mysql", "root:******@tcp(127.0.0.1:3306)/test_lucky")
+		if err != nil {
+			fmt.Fprintf(w, err.Error())
+			// panic(err)
+		}
 
-  // fmt.Fprintf(w,"('%s','%s','%s')",title,announcement,full_text)  
-  http.Redirect(w, r, "/", http.StatusSeeOther)
+		defer db.Close()
+
+		insert, err := db.Query(fmt.Sprintf("insert into `articles` (`title`,`announcement`,`full_text`) values ('%s','%s','%s')", title, announcement, full_text))
+		if err != nil {
+			fmt.Fprintf(w, err.Error())
+			// panic(err)
+		}
+
+		defer insert.Close()
+	}
+
+	// fmt.Fprintf(w,"('%s','%s','%s')",title,announcement,full_text)
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
-
 
 func handleFunc() {
-  http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
-  http.HandleFunc("/",index)
-  http.HandleFunc("/create",create)
-  http.HandleFunc("/save_article",save_article)
-  http.ListenAndServe(":8080",nil)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+	http.HandleFunc("/", index)
+	http.HandleFunc("/create", create)
+	http.HandleFunc("/save_article", save_article)
+	http.ListenAndServe(":8080", nil)
 }
 
-func main () {
-  handleFunc()
+func main() {
+	configuration := Configuration{}
+	err := gonfig.GetConf("./cfg/main.cfg", &configuration)
+	if err != nil {
+		panic(err)
+	}
+	handleFunc()
 }
